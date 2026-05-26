780c4b495fed9c32e62d3caa3344f52b999ce88e
Muhammad Yasir <muhammad.yasir@elnusa.co.id>
2026-05-26 14:39:10 +0700

    feat: add category entity and transaction type enum

---
+++ src/02.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using Obscura.FinanceTracker.Base.Entities;
+using Obscura.FinanceTracker.Domain.Enums;
+
+namespace Obscura.FinanceTracker.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public TransactionType Type { get; set; }
+    }
+}
---
+++ src/02.Domain/Enums/TransactionType.cs
@@ -0,0 +1,8 @@
+﻿namespace Obscura.FinanceTracker.Domain.Enums
+{
+    public enum TransactionType
+    {
+        Income = 1,
+        Expense = 2
+    }
+}
