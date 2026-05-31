21617f650e536d86e91ed3cb52eecbb63a5b8737
Muhammad Yasir <muhammad.yasir@elnusa.co.id>
2026-05-31 23:33:39 +0700

    feat: implement category api client

---
+++ src/07.Client/Categories/CategoryClient.cs
@@ -0,0 +1,41 @@
+﻿using Obscura.FinanceTracker.Application.Categories.DTOs;
+using Obscura.FinanceTracker.Application.Categories.Requests;
+using System.Net.Http.Json;
+
+namespace Obscura.FinanceTracker.Client.Categories
+{
+    public class CategoryClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoryClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<CategoryDto>> GetAllAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<CategoryDto>>("api/categories") ?? [];
+        }
+
+        public async Task<CategoryDto> GetByIdAsync(Guid id)
+        {
+            return await _httpClient.GetFromJsonAsync<CategoryDto>("api/categories/" + id) ?? new CategoryDto();
+        }
+
+        public async Task CreateAsync(CreateCategoryRequest createCategoryRequest)
+        {
+            await _httpClient.PostAsJsonAsync("api/categories", createCategoryRequest);
+        }
+
+        public async Task UpdateAsync(Guid id, UpdateCategoryRequest request)
+        {
+            await _httpClient.PutAsJsonAsync($"api/category/{id}", request);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await _httpClient.DeleteAsync($"api/category/{id}");
+        }
+    }
+}
---
+++ src/07.Client/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Obscura.FinanceTracker.Client.Categories;
+
+namespace Obscura.FinanceTracker.Client;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddClientServices(this IServiceCollection services)
+    {
+        services.AddScoped<CategoryClient>();
+
+        return services;
+    }
+}
