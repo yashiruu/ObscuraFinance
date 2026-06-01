c485aa177d726cf71c5078572b582af8d299e3c3
Muhammad Yasir <muhammad.yasir@elnusa.co.id>
2026-06-01 22:35:07 +0700

    feat (domain): add account entity and type enum

---
+++ src/02.Domain/Entities/Account.cs
@@ -0,0 +1,16 @@
+﻿using Obscura.FinanceTracker.Base.Entities;
+using Obscura.FinanceTracker.Domain.Enums;
+
+namespace Obscura.FinanceTracker.Domain.Entities
+{
+    public class Account : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public decimal InitialBalance { get; set; } = Decimal.Zero;
+        public decimal CurrentBalance { get; set; } = Decimal.Zero;
+        public string Currency { get; set; } = "IDR";
+        public AccountType Type { get; set; } = AccountType.Bank;
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ src/02.Domain/Enums/AccountType.cs
@@ -0,0 +1,11 @@
+﻿namespace Obscura.FinanceTracker.Domain.Enums
+{
+    public enum AccountType
+    {
+        Bank = 0,
+        EWallet = 1,
+        Cash = 2,
+        Investment = 3,
+        CreditCard = 4,
+    }
+}
