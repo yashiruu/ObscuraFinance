97c34210badc322529d0fd324164a862abd0dde2
Muhammad Yasir <muhammad.yasir@elnusa.co.id>
2026-06-20 23:24:48 +0700

    feat(application): add interfaces as contract for service

---
+++ src/04.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using Obscura.FinanceTracker.Application.DTOs.Categories.Requests;
+using Obscura.FinanceTracker.Application.DTOs.Categories.Responses;
+
+namespace Obscura.FinanceTracker.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryResponse>> GetAllAsync(CancellationToken cancellationToken);
+        Task<IEnumerable<CategoryResponse>> GetByTypeAsync(int type, CancellationToken cancellationToken);
+        Task<IEnumerable<CategoryResponse>> GetDeletedAsync(CancellationToken cancellationToken);
+        Task<CategoryResponse?> GetByIdASync(Guid id, CancellationToken cancellationToken);
+        Task CreateAsync(CategoryCreateRequest request ,CancellationToken cancellationToken);
+        Task UpdateAsync(Guid id, CategoryUpdateRequest request, CancellationToken cancellationToken);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken);
+        Task RestoreASync(Guid id, CancellationToken cancellationToken);
+    }
+}
---
+++ src/04.Application/Interfaces/IDashboardSummary.cs
@@ -0,0 +1,14 @@
+﻿using Obscura.FinanceTracker.Application.DTOs.Dashboard.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obscura.FinanceTracker.Application.Interfaces
+{
+    public interface IDashboardSummary
+    {
+        Task<DashboardSummaryResponse> GetDashboardSummaryAsync(CancellationToken cancellationToken);
+    }
+}
