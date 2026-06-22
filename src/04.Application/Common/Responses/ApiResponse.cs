eb01a772c0d0c4aae5a79bfa53b03d1ac675e515
Muhammad Yasir <muhammad.yasir@elnusa.co.id>
2026-06-22 09:22:37 +0700

    feat(api): add standardized ApiResponse wrapper

---
+++ src/04.Application/Common/Responses/ApiResponse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obscura.FinanceTracker.Application.Common.Responses
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+        // Success Response
+        public static ApiResponse<T> SuccessResponse(T data, string message = "Sucess")
+        {
+            return new ApiResponse<T>
+            {
+                Success = true,
+                Message = message,
+                Data = data
+            };
+        }
+
+        // Error Response
+        public static ApiResponse<T> ErrorResponse(string message)
+        {
+            return new ApiResponse<T>
+            {
+                Success = false,
+                Message = message,
+                Data = default
+            };
+        }
+    }
+}
